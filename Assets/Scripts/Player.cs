63c837c787df7ab35c94c2d7f86d37457690adfe
Alex Menelli <menellialex@gmail.com>
2023-04-25 13:27:20 -0400

    Link has animations
    They are broken, but they are there.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,15 +10,47 @@ public class Player : MonoBehaviour
     public float runSpeed = 10f;
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public Sprite runRight;
+    public Sprite runLeft;
+    public Sprite idle;
+    public Sprite swing;
+    public float swingTimer;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
 
+        if (movement.x > 0)
+        {
+            //display link running right
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = runRight;
+            swingTimer = 0;
+        }
+        else if (movement.x < 0)
+        {
+            //running left
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = runLeft;
+            swingTimer = 0;
+        }
+        else
+        {
+            //idle
+            if (swingTimer == 0)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = idle;
+            }
+            else
+            {
+                swingTimer--;
+            }
+        }
+
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = swing;
+            swingTimer = 10;
         }
     }
 
